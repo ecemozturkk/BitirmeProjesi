e75843156172fca620a3d6d31113f802adc3a0ee
ecemozturkk <ecemozturk704@gmail.com>
2023-05-15 22:28:30 +0300

    Initial Commit

---
+++ BitirmeProjesi/BitirmeProjesiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitirmeProjesiApp.swift
+//  BitirmeProjesi
+//
+//  Created by Ecem Öztürk on 15.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BitirmeProjesiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitirmeProjesi/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BitirmeProjesi
+//
+//  Created by Ecem Öztürk on 15.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
